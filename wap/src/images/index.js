4a5f630de3588fac781e0359a8263564d439a706
代汉桥 <dhq@daihanqiaodeMacBook-Pro.local>
2018-01-10 22:34:32 +0800

    商品详情

--- wap/src/images/index.js
+++ wap/src/images/index.js
@@ -2,4 +2,14 @@ export const icon = {
     back:require('./icon/backIcon.png'),
     cart:require('./icon/shopCart.png'),
     share:require('./icon/shareIcon.png'),
+};
+
+export const test = {
+    test1:require('./test/1.jpg'),
+    test2:require('./test/2.jpg'),
+    test3:require('./test/3.jpg'),
+    test4:require('./test/4.png'),
+    test5:require('./test/5.png'),
+    test6:require('./test/6.png'),
+    test7:require('./test/7.png'),
 }
