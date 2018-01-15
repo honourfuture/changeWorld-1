163a87f3f31a42f25385616a78e359ccea8ec5a8
daihanqiao@126.com <daihanqiao@126.com>
2018-01-15 15:53:53 +0800

    测试

--- wap/src/panels/ExLog/index.js
+++ wap/src/panels/ExLog/index.js
@@ -7,6 +7,7 @@ import {icon} from '../../images';
 const Step = Steps.Step;
 export default class ExLog extends BaseComponent{
 	render(){
+		const tel = 18058769605;
 		return (
 			<div className='ExLog'>
 				<NavBar
@@ -31,8 +32,7 @@ export default class ExLog extends BaseComponent{
                 	<div className="exStep">
                 		<Steps size="small">
 					      	<Step status="error" className="last" title="【杭州市】已签收，签收人：前台代收" description="2017-04-14 03:52:12" />
-					      	<Step status="process" title={<div>【杭州市】五常揽投站安排投递，预计13:00:00前投递（投递员 姓名：周文进；联系电话：<a href='tel:18058769605'>18058769605</a></div>} description="2017-04-14 03:52:12" />
-					      	<Step status="process" title={()=><div>【杭州市】五常揽投站安排投递，预计13:00:00前投递（投递员 姓名：周文进；联系电话：<span>18058769605</span>）</div>} description="2017-04-14 03:52:12" />
+					      	<Step status="process" title={<div>【杭州市】五常揽投站安排投递，预计13:00:00前投递（投递员 姓名：周文进；联系电话：<a href={'tel:'+tel}>{tel}</a></div>} description="2017-04-14 03:52:12" />
 					      	<Step status="process" title="【杭州市】离开杭州处理中心 发往五常揽投站" description="2017-04-14 03:52:12" />
 					      	<Step status="process" title="【杭州市】已揽收" description="2017-04-14 03:52:12" />
 					      	<Step status="process" title="包裹已出库来自：杭州报税1号仓" description="2017-04-14 03:52:12" />
