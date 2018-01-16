ffe90c338782cd712dabbf601c16effa41d73609
comcjl052@qq.com <comcjl052@qq.com>
2018-01-16 22:10:19 +0800

    我的茶农

---
+++ wap/src/panels/SalesLog/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BaseComponent,Base} from '../../common';
+import './SalesLog.less';
+
+export default class SalesLog extends BaseComponent{
+	render(){
+		return (
+			<div className='SalesLog'>
+			</div>
+		)
+	}
+};
