2ed452362a1f52481fc33c3cb02f38e74836c123
代汉桥 <dhq@daihanqiaodeMacBook-Pro.local>
2018-01-17 22:02:40 +0800

    优化

--- wap/src/panels/NewAddress/index.js
+++ wap/src/panels/NewAddress/index.js
@@ -9,15 +9,8 @@ import {icon} from '../../images';
 import { district } from '../../common/cityData';
 
 const Item = List.Item;
-// @createForm
 class NewAddress extends BaseComponent{
-    state={
-        sValue: [],
-        visible: false,
-        checked: false,
-    }
-    componentDidMount(){
-    }
+    store={sValue:[],visible:false,checked:false}
     @action.bound
     onSave(){
         this.props.form.validateFields((err, values) => {
@@ -28,23 +21,20 @@ class NewAddress extends BaseComponent{
                 const areaData = sValue.length === 3 ? cityData.children.find(item=>sValue[2] === item.value) : "";
                 let isDefault = checked?1:0;
                 Base.GET({act:'address',op:'save',mod:'',id:'0',is_default:isDefault,username:values.userName,mobi:values.userPhone,province_id:provinceData.value,province:provinceData.label,city_id:cityData.value,city:cityData.label,area_id:areaData.value || 0,area:areaData.label || "",address:values.userAddress},(res)=>{
-                    if(res.status === 0){
-                        Toast.success(res.message, 1);
-                    }else{
-                        Toast.offline(res.message, 1);
-                    }
+                    Toast.success(res.message);
                 });
             }
         });
     }
-    // validatePhone(value){
-    //     console.log(value);
-    //         return true;
-    // }
+    @action.bound
+    onSelect(v){
+        this.store.sValue = v;
+        this.store.visible = true;
+    }
     render(){
         const { getFieldProps, getFieldError } = this.props.form;
-        const {sValue,visible,checked,sName,sPhone,sAddress} = this.state;
-        let takeRegion = visible ? "take-region check-address" : "take-region";
+        const {sValue,visible,checked} = this.store;
+        const takeRegion = visible ? "take-region check-address" : "take-region";
         return (
             <div className='NewAddress'>
                 <NavBar
@@ -69,8 +59,7 @@ class NewAddress extends BaseComponent{
                             error={getFieldError('userPhone')}
                             {...getFieldProps('userPhone', {
                                 rules: [
-                                  { required: true, message: '请输入收货人的手机号码' },
-                                  // { validator: this.validatePhone },
+                                  { required: true, message: '请输入收货人的手机号码',pattern:/^1(3|4|5|7|8)\d(\ )?\d{4}(\ )?\d{4}$/ },
                                 ],
                             })}
                             clear
@@ -86,11 +75,10 @@ class NewAddress extends BaseComponent{
                                 {...getFieldProps('userArea', {
                                     rules: [
                                       { required: true, message: '请选择区域' },
-                                      // { validator: this.validateAccount },
                                     ],
                                 })}
                                 dismissText=" "
-                                onOk={v =>this.setState({sValue:v,visible:true})}
+                                onOk={this.onSelect}
                                 value={sValue}
                                 >
                                 <Item arrow="horizontal">省市区</Item>
