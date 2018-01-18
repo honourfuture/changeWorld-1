fdf7eb9d3aecfc12ec92bba1b269ed7a527eb3b6
daihanqiao@126.com <daihanqiao@126.com>
2018-01-18 16:07:28 +0800

    商品发布

--- wap/src/panels/ProductIssue/index.js
+++ wap/src/panels/ProductIssue/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { action } from 'mobx';
 import { createForm } from 'rc-form';
 import {BaseComponent,Base} from '../../common';
-import {Flex,Button,NavBar,WhiteSpace,List,InputItem,Switch,ImagePicker,TextareaItem,WingBlank,Toast,Picker} from 'antd-mobile';
+import {Flex,Button,NavBar,WhiteSpace,List,InputItem,Switch,ImagePicker,WingBlank,Toast,Picker} from 'antd-mobile';
 import './ProductIssue.less';
 import {icon} from '../../images';
 
@@ -11,23 +11,22 @@ const Item = List.Item;
 class ImgItem extends BaseComponent {
 	@action.bound
 	onChangeImg(files, type, index){
-		// const {callBack} = this.props;
-        // callBack && callBack(files,type, index);
-        console.log(files);
+        const {callBack} = this.props;
         if(type === 'add'){
-            Base.POST({act:'common',op:'base64FileUpload',base64_image_content:files[files.length - 1].url},(res)=>{
-                
+            const requestFile = files[files.length - 1];
+            Base.POST({act:'common',op:'base64FileUpload',base64_image_content:requestFile.url},(res)=>{
+                requestFile.file_url = res.data.file_url;
+                callBack && callBack(files);
             })
+        }else{
+            callBack && callBack(files);
         }
 	}
     render(){
-    	const {title,fileName,isTextArea} = this.props;
-
-    	const textArea = isTextArea === true ? <TextareaItem autoHeight placeholder="请输入产品描述会直接上传产品详情图片" className="productDetailInfo" /> : "";
+    	const {title,fileName,isRequired} = this.props;
         return (
             <div className="productImg">
-            	<div className="mainTit">{title}<em>*</em></div>
-            	{textArea}
+            	<div className="mainTit">{title}{isRequired?<em>*</em>:null}</div>
             	<ImagePicker
 			        files={fileName}
 			        onChange={this.onChangeImg}
@@ -42,36 +41,52 @@ class ImgItem extends BaseComponent {
 }
 
 class ProductIssue extends BaseComponent{
-	state={
-		checked: false,
-	}
 	store={
 		goods_image:[],
-        productDetail:[],
+        goods_detail:[],
         send_mode:[],
         goods_attr:[],
     }
-    // _request
 	@action.bound
-	onChangeMainImg = (files, type, index) => {
-        console.log(files, type, index);
+	onChangeMainImg = (files) => {
+        console.log(files);
 	    this.store.goods_image = files;
 	}
 	@action.bound
-    onChangeDetailImg = (files, type, index) => {
-        console.log(files, type, index);
-	    this.store.productDetail = files;
+    onChangeDetailImg = (files) => {
+	    this.store.goods_detail = files;
     }
     @action.bound
     onSubmit(){
         this.props.form.validateFields((err, values) => {
             if(!err){
-                console.log(values);
-                const {send_mode} = values;
+                const {send_mode,full_amount=0,free_amount=0} = values;
                 if(!send_mode){
-                    return Toast.fail('请选择发货模式');
+                    return Toast.fail('请选择发货模式',2,null,false);
                 }
-                Base.POST({act:'goods',op:'save',...values,send_mode:send_mode[0]},(res)=>{
+                const {goods_image,goods_detail} = this.store;
+                if(goods_image.length === 0){
+                    return Toast.fail('请上传产品主图',2,null,false);
+                }
+                const goods_imageUrl = goods_image.map(item=>item.file_url);
+                const goods_detailUrl = goods_detail.map(item=>item.file_url);
+                const goods_attr = {};
+                for (const key in values) {
+                    if (values.hasOwnProperty(key) && key.indexOf('attr_') !== -1) {
+                        if(values[key]){
+                            const [str,id,index] = key.split('_');
+                            goods_attr[id] = goods_attr[id] || [];
+                            goods_attr[id][index] = values[key];
+                        }
+                        delete values[key];
+                    }
+                }
+                const goods_ticket = JSON.stringify([{full_amount,free_amount}]);
+                const e_invoice = values.e_invoice?1:0;
+                delete values['full_amount'];
+                delete values['free_amount'];
+                delete values['e_invoice'];
+                Base.POST({act:'goods',op:'save',...values,e_invoice,send_mode:send_mode[0],goods_ticket,goods_image:JSON.stringify(goods_imageUrl),goods_detail:JSON.stringify(goods_detailUrl),goods_attr:JSON.stringify(goods_attr)},(res)=>{
 
                 })
             }else{
@@ -86,19 +101,46 @@ class ProductIssue extends BaseComponent{
             }
         });
     }
+    @action.bound
+    onAddAttr(index){
+        this.store.goods_attr[index].list.push('');
+    }
     componentDidMount(){
         Base.GET({act:'goods',op:'init'},(res)=>{
             const {goods_attr,send_mode} = res.data;
-            this.store.goods_attr = goods_attr;
+            this.store.goods_attr = goods_attr.map((item)=>{
+                return {...item,list:[]};
+            });
             this.store.send_mode = send_mode.map((label,value)=>{
                 return {label,value};
             });
         })
     }
 	render(){
-		const {checked} = this.state;
-        const {goods_image,productDetail,send_mode} = this.store;
+        const {goods_image,goods_detail,send_mode,goods_attr} = this.store;
         const { getFieldProps, getFieldError } = this.props.form;
+        const goodsAttrItems = goods_attr.map((item,index)=>{
+            const {id,name,list} = item;
+            return (
+                <List key={id} className="productBasic attr-con">
+                    <Item
+                        className='attr-item'
+                        extra={<Button type='ghost' className='add-btn' size='small' onClick={()=>this.onAddAttr(index)}>添加</Button>}
+                    >{name}<em>{'>'}</em></Item>
+                    {
+                        list.map((attrItem,attrIndex)=>{
+                            const propsStr = `attr_${id}_${attrIndex}`;
+                            return <InputItem 
+                                key={attrIndex}
+                                {...getFieldProps(propsStr,{initialValue:attrItem})}
+                                clear
+                                placeholder="请输入属性值"
+                            ></InputItem>
+                        })
+                    }
+                </List>
+            )
+        })
 		return (
 			<div className='ProductIssue'>
 				<NavBar
@@ -179,19 +221,19 @@ class ProductIssue extends BaseComponent{
                                 </Flex>
                             }
                         >
-                        发货模式</Item>
+                        优惠</Item>
                         <InputItem
                             {...getFieldProps('use_point_rate')}
                             clear
                             type="number"
-                            placeholder="0%"
+                            placeholder="0"
                             moneyKeyboardAlign="right"
+                            extra="%"
                         >积分使用比例</InputItem>
                         <Item
                           extra={<Switch 
-                                {...getFieldProps('e_invoice')}
-                                onClick={() => this.setState({checked:!checked})} 
-                                size="small" checked={checked} 
+                                {...getFieldProps('e_invoice',{valuePropName: 'checked'})}
+                                size="small"
                                 color="red" 
                             />}
                         >是否支持电子发票</Item>
@@ -202,22 +244,24 @@ class ProductIssue extends BaseComponent{
                             {...getFieldProps('city_partner_rate')}
                             clear
                             type="number"
-                            placeholder="0%"
+                            placeholder="0"
                             moneyKeyboardAlign="right"
+                            extra="%"
                         >城市合伙人分销比例</InputItem>
                         <InputItem 
                             {...getFieldProps('two_level_rate')}
                             clear
                             type="number"
-                            placeholder="0%"
+                            placeholder="0"
                             moneyKeyboardAlign="right"
+                            extra="%"
                         >二级分销比例</InputItem>
                     </List>
                     <WhiteSpace />
-                    <ImgItem title={'产品主图'} fileName={goods_image} callBack={this.onChangeMainImg} />
+                    <ImgItem title={'产品主图'} isRequired={true} fileName={goods_image} callBack={this.onChangeMainImg} />
                     <WhiteSpace />
-                    <Flex align="center" justify="center" style={{padding:20}}>预留产品属性</Flex>
-                    <ImgItem title={'产品详情'} isTextArea={true} fileName={productDetail} callBack={this.onChangeDetailImg} />
+                    {goodsAttrItems}
+                    <ImgItem title={'产品详情'} fileName={goods_detail} callBack={this.onChangeDetailImg} />
                     <WhiteSpace size="xl" />
                     <WingBlank>
                         <Button onClick={this.onSubmit} type="warning" className="save-address">提交</Button>
