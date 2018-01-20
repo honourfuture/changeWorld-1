6924a3ce24ef44d99be41a484dcf6e47a416d980
35945042 <webljx@gmail.com>
2018-01-20 22:11:54 +0800

    阅读记录

---
+++ php/application/models/Users_read_model.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * 会员阅读表
+ * @author sz.ljx
+ * @author webljx@163.com
+ * @link www.aicode.org.cn
+ */
+
+class Users_read_model extends MY_Model
+{
+    public $_table        = 'users_read';
+    public $primary_key   = 'id';
+    public $before_create = array();
+    public $before_create = array('created_at', 'updated_at', 'updated_valid');
+    public $before_update = array('updated_at', 'updated_valid');
+    public $protected_attributes = array('id');
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function initType()
+    {
+        return array(
+            1 => '会员消息'
+        );
+    }
+
+    public function save($user_id, $type, $id)
+    {
+        if($user_id && $type && $id){
+            $where = array('user_id' => $user_id, 'type' => $type);
+            $info = $this->get_by($where);
+            if($info){
+                if(strpos($info['ids'], ','.$id.',') === false){
+                    return $this->update($info['id'], array('ids' => $info['ids'].$id.','));
+                }else{
+                    return true;
+                }
+            }else{
+                $where['ids'] = ','.$id.',';
+                return $this->insert($where);
+            }
+        }
+
+        return false;
+    }
+}
