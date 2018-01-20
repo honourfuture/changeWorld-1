8d53aa7e4f4a354f1bda396df1fa87147559ba8d
comcjl052@qq.com <comcjl052@qq.com>
2018-01-20 16:24:18 +0800

    商品管理

--- admin/src/panels/GoodsManager/index.js
+++ admin/src/panels/GoodsManager/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {action} from 'mobx';
 import {BaseComponent,Base} from '../../common';
-import { Table, Input,Popconfirm,Switch,Button,Spin,message,DatePicker } from 'antd';
+import { Table, Input,Popconfirm,Switch,Button,Spin,message,Select } from 'antd';
 import './GoodsManager.less';
 import {remove} from 'lodash';
+const Option = Select.Option;
 
 const EditableCell = ({ editable, value, onChange, type}) => (
 	<div>
@@ -14,44 +15,78 @@ const EditableCell = ({ editable, value, onChange, type}) => (
 	</div>
 );
 
+// const EditableCellSelect = ({ editable, value, onChange, type}) => (
+// 	<div>
+// 		{editable
+// 			? <Select >
+// 			      <Option value="jack">Jack</Option>
+// 			      <Option value="lucy">Lucy</Option>
+// 			      <Option value="Yiminghe">yiminghe</Option>
+// 			    </Select>
+// 			: value
+// 		}
+// 	</div>
+// );
+
 
 export default class GoodsManager extends BaseComponent{
 	store={
 		list:[],
+		user:{},
+		goodsClass:[],
 	}
 	constructor(props) {
 		super(props);
 		this.columns = [
 			{
-				title: '序号',
-				dataIndex: 'id',
+				title: 'sort',
+				dataIndex: 'sort',
 				width: '10%',
-				render: (text, record) => this.renderText(text, record, 'id'),
+				render: (text, record) => this.renderColumns(text, record, 'sort'),
 			}, 
 			{
+				title: '发布时间',
+				dataIndex: 'updated_at',
+				width: '10%',
+				render: (text, record) => this.renderText(text, record, 'updated_at'),
+			},
+			{
+				title: '发布人',
+				dataIndex: 'seller_uid',
+				width: '10%',
+				render: (text, record) => this.renderStoreInfo(text, record, 'seller_uid'),
+			},
+			{
 				title: '商品图片',
-				dataIndex: 'title',
-				render: (text, record) => this.renderColumns(text, record, 'title'),
+				dataIndex: 'default_image',
+				width: '10%',
+				render: (text, record) => this.renderImg(text, record, 'default_image'),
 			},
 			{
 				title: '商品名称',
-				dataIndex: 'title',
-				render: (text, record) => this.renderColumns(text, record, 'title'),
+				dataIndex: 'name',
+				width: '10%',
+				render: (text, record) => this.renderText(text, record, 'name'),
 			},
 			{
-				title: '规格',
-				dataIndex: 'title',
-				render: (text, record) => this.renderColumns(text, record, 'title'),
+				title: '分类',
+				dataIndex: 'shop_class_id',
+				render: (text, record) => this.renderColumnSelect(text, record, 'shop_class_id'),
 			}, 
 			{
 				title: '库存',
-				dataIndex: 'title',
-				render: (text, record) => this.renderColumns(text, record, 'title'),
+				dataIndex: 'stock',
+				render: (text, record) => this.renderText(text, record, 'stock'),
+			}, 
+			{
+				title: '售价',
+				dataIndex: 'sale_price',
+				render: (text, record) => this.renderText(text, record, 'sale_price'),
 			}, 
 			{
 				title: '上/下架',
 				dataIndex: 'enable',
-				width: '15%',
+				width: '10%',
 				render: (text, record) => this.renderSwitch(text, record, 'enable'),
 			}, 
 			{
@@ -70,7 +105,8 @@ export default class GoodsManager extends BaseComponent{
 							</span>
 							:
 							<span>
-								<a onClick={() => this.onEditChange(id,true,'editable')}>编辑</a>
+								<a onClick={() => this.onEditChange(id,true,'editable')}>编辑</a>&nbsp;&nbsp;
+								<a>查看</a>
 								<Popconfirm title="确认删除?" okText='确定' cancelText='取消' onConfirm={() => this.onDelete(id)}>
 									<a className='ml10 gray'>删除</a>
 								</Popconfirm>
@@ -82,11 +118,25 @@ export default class GoodsManager extends BaseComponent{
 			}
 		];
 	}
+	renderStoreInfo(text,record,column){
+		const {user} = this.store;
+		return (
+			<div>
+				{user[text].nickname}
+			</div>
+		)
+	}
 	renderText(text, record, column) {
 		return (
 			<div>
 				{record[column]}
 			</div>
+		);
+	}
+	renderImg(text, record, column) {
+		const {default_image} = record;
+		return (
+			<img className='goodsImg' src={default_image} alt=''/>
 		);
 	}
 	renderColumns(text, record, column) {
@@ -98,6 +148,20 @@ export default class GoodsManager extends BaseComponent{
 				onChange={value => this.onEditChange(record.id, value, column)}
 			/>
 		);
+	}
+	renderColumnSelect(text, record, column) {
+		const {editable} = record;
+		const {goodsClass} = this.store;
+		const value = record[column];
+		let curIndex = goodsClass.findIndex((item)=>item.id === value);
+		curIndex = curIndex >= 0?curIndex:0;
+		return <div>
+				{editable?<Select defaultValue={value|| goodsClass[0].id} style={{ width: 120 }} onChange={(value)=>this.onEditChange(record.id,value,column)}>
+					{
+						goodsClass.map(({id,name})=><Option key={id} value={id}>{name}</Option>)
+					}
+				</Select>:goodsClass[curIndex].name}
+			</div>
 	}
 	renderSwitch(text,record,column){
 		return (
@@ -118,7 +182,7 @@ export default class GoodsManager extends BaseComponent{
 		const list = this.store.list.slice();
 		const itemData = list.find(item=>id === item.id);
 		itemData.editable = false;
-		Base.POST({act:'security_question',op:'save',...itemData},(res)=>{
+		Base.POST({act:'goods',op:'save',...itemData},(res)=>{
 			itemData.updated_at = Base.getTimeFormat(new Date().getTime()/1000,2);
 			itemData.id === 0 && (itemData.id = res.data.id);
 			this.store.list = list;
@@ -131,43 +195,37 @@ export default class GoodsManager extends BaseComponent{
 		const list = this.store.list.slice();
 		const itemData = list.find(item=>id === item.id);
 		itemData[column] = value;
-		Base.POST({act:'security_question',op:'save',...itemData},()=>this.store.list = list,this);
+		Base.POST({act:'goods',op:'save',...itemData},()=>this.store.list = list,this);
 	}
 	//取消
 	@action.bound
 	onCancel(id) {
 		this.store.list = this.cacheData.map(item => ({ ...item }));
 	}
-	//添加
-	// @action.bound
-	// onAdd(){
-	// 	if(this.store.list.find(item=>item.id === 0)){
-	// 		return message.info('请保存后再新建');
-	// 	}
-	// 	this.store.list.unshift({id:'',title:'',editable:true,deleted:'0',enable:'1'});
-	// }
 	//删除
 	@action.bound
 	onDelete(id){
-		Base.POST({act:'security_question',op:'save',id,deleted:"1"},()=>remove(this.store.list,item=>id === item.id),this);
+		Base.POST({act:'goods',op:'save',id,deleted:"1"},()=>remove(this.store.list,item=>id === item.id),this);
 	}
 	componentDidMount() {
 		Base.GET({act:'goods',op:'index'},(res)=>{
-			console.log(res,"Res")
-			const {list} = res.data;
-			this.store.list = list;
-			this.cacheData = list.map(item => ({ ...item }));
+			const {goods,user} = res.data;
+			this.store.list = goods.list;
+			this.store.user = user;
+			this.cacheData = goods.list.map(item => ({ ...item }));
+		},this);
+		Base.GET({act:'shop_class',op:'index'},(res)=>{
+			this.store.goodsClass = res.data;
 		},this);
 	}
 	render(){
 		let {list} = this.store;
-		// const showList = list.filter(item=>{
-		// 	return parseInt(item.deleted,10) === 0;
-		// })
+		const showList = list.filter(item=>{
+			return parseInt(item.deleted,10) === 0;
+		});
 		return (
 			<div className='GoodsManager'>
-				{/*<Button onClick={this.onAdd}>新增+</Button>*/}
-				<Table className="mt16" bordered dataSource={list} rowKey='id' columns={this.columns} pagination={false} />
+				<Table className="mt16" bordered dataSource={showList} rowKey='id' columns={this.columns} pagination={false} />
 			</div>
 		)
 	}
