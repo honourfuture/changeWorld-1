312ac1aa4d08e2be7a00ea61d82e9392bdfdb87d
35945042 <webljx@gmail.com>
2018-03-01 13:26:18 +0800

    专辑类型、标签

---
+++ php/application/controllers/api/Album_class.php
@@ -0,0 +1,167 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * @author sz.ljx
+ * @email webljx@163.com
+ * @link www.aicode.org.cn
+ */
+class Album_class extends API_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Album_class_model');
+    }
+
+    /**
+	 * @api {get} /api/album_class 专辑类型-列表
+	 * @apiVersion 1.0.0
+	 * @apiName album_class
+	 * @apiGroup api
+	 *
+	 * @apiSampleRequest /api/album_class
+	 *
+	 * @apiParam {Number} user_id 用户唯一ID
+	 * @apiParam {String} sign 校验签名
+	 *
+	 * @apiSuccess {Number} status 接口状态 0成功 其他异常
+	 * @apiSuccess {String} message 接口信息描述
+	 * @apiSuccess {Object[]} data 接口数据集
+	 * @apiSuccess {String} data.id 专辑类型唯一ID
+	 * @apiSuccess {String} data.name 专辑类型名称
+	 *
+	 * @apiSuccessExample {json} Success-Response:
+	 * {
+	 *	    "data": [
+	 *	        {
+	 *	            "id": "1",
+	 *	            "name": "热门"
+	 *	        },
+	 *	        {
+	 *	            "id": "2",
+	 *	            "name": "靓号"
+	 *	        }
+	 *	    ],
+	 *	    "status": 0,
+	 *	    "message": "成功"
+	 *	}
+	 *
+	 * @apiErrorExample {json} Error-Response:
+	 * {
+	 * 	   "data": "",
+	 *     "status": -1,
+	 *     "message": "签名校验错误"
+	 * }
+	 */
+	public function index()
+	{
+		$deleted = (int)$this->input->get('deleted');
+		if($this->user_id){
+			$this->db->select('id,name');
+		}
+		$order_by = array('sort' => 'desc', 'id' => 'desc');
+		$ret = $this->Album_class_model->order_by($order_by)->get_many_by('deleted', $deleted);
+		$this->ajaxReturn($ret);
+	}
+
+	// 查看
+	public function view()
+	{
+
+	}
+
+	/**
+	 * @api {post} /api/album_class/save 专辑类型-编辑 OR 新增
+	 * @apiVersion 1.0.0
+	 * @apiName album_class_save
+	 * @apiGroup admin
+	 *
+	 * @apiSampleRequest /api/album_class/save
+	 *
+	 * @apiParam {Number} admin_id 管理员唯一ID
+	 * @apiParam {String} account 登录账号
+	 * @apiParam {String} sign 校验签名
+	 * @apiParam {Number} id 记录唯一ID 0表示新增 其他表示编辑
+	 * @apiParam {String} name 专辑类型名称
+	 * @apiParam {Number} sort 排序 降序排列
+	 * @apiParam {Number} enable 启用 1是 0否
+	 * @apiParam {Number} deleted 是否删除 1是 0否（为1时其他字段可不传）
+	 *
+	 * @apiSuccess {Number} status 接口状态 0成功 其他异常
+	 * @apiSuccess {String} message 接口信息描述
+	 * @apiSuccess {String} data 接口数据集
+	 *
+	 * @apiSuccessExample {json} Success-Response:
+	 * {
+	 *	    "data": "",
+	 *	    "status": 0,
+	 *	    "message": ""
+	 *	}
+	 *
+	 * @apiErrorExample {json} Error-Response:
+	 * {
+	 * 	   "data": "",
+	 *     "status": -1,
+	 *     "message": "签名校验错误"
+	 * }
+	 */
+	public function save()
+	{
+		$this->check_operation();
+		$id = (int)$this->input->get_post('id');
+		if($id){
+			$params = elements(
+				array(
+					'name', 'sort', 'deleted', 'enable'
+				),
+				$this->input->post(),
+				UPDATE_VALID
+			);
+			$this->check_params('edit', $params);
+			if($params['deleted'] == 1){
+				$update = array('deleted' => 1, 'enable' => 0);
+				$flag = $this->Album_class_model->update($id, $update);
+			}else{
+				unset($params['deleted']);
+				if(isset($params['enable']) && $params['enable']){
+					$params['deleted'] = 0;
+				}
+				$flag = $this->Album_class_model->update($id, $params);
+			}
+		}else{
+			$params = elements(
+				array(
+					'name', 'sort'
+				),
+				$this->input->post(),
+				UPDATE_VALID
+			);
+			$this->check_params('add', $params);
+			if($flag = $this->Album_class_model->insert($params)){
+				$id = $flag;
+			}
+		}
+
+		if($flag){
+			$status = 0;
+			$message = '成功';
+		}else{
+			$status = 1;
+			$message = '失败';
+		}
+		$this->ajaxReturn(array('id' => $id), $status, '操作'.$message);
+	}
+
+	protected function check_params($act, $params)
+	{
+		switch($act){
+			case 'add':
+				if($params['name'] === '' || $params['name'] == UPDATE_VALID){
+					$this->ajaxReturn([], 501, '名称参数错误');
+				}
+				break;
+			case 'edit':
+				break;
+		}
+	}
+}
---
+++ php/application/controllers/api/Album_tag.php
@@ -0,0 +1,202 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * @author sz.ljx
+ * @email webljx@163.com
+ * @link www.aicode.org.cn
+ */
+class Album_tag extends API_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Album_tag_model');
+    }
+
+    /**
+	 * @api {get} /api/album_tag 专辑标签-列表
+	 * @apiVersion 1.0.0
+	 * @apiName album_tag
+	 * @apiGroup api
+	 *
+	 * @apiSampleRequest /api/album_tag
+	 *
+	 * @apiParam {Number} user_id 用户唯一ID
+	 * @apiParam {String} sign 校验签名
+	 *
+	 * @apiSuccess {Number} status 接口状态 0成功 其他异常
+	 * @apiSuccess {String} message 接口信息描述
+	 * @apiSuccess {Object} data 接口数据集
+	 *
+	 * @apiSuccessExample {json} Success-Response:
+	 * {
+	 *     "data": {
+	 *         "1": {
+	 *             "name": "内容",
+	 *             "limit": "3",
+	 *             "list": [
+	 *                 {
+	 *                     "6": "广告"
+	 *                 },
+	 *                 {
+	 *                     "5": "铃声"
+	 *                 },
+	 *                 {
+	 *                     "4": "配音"
+	 *                 },
+	 *                 {
+	 *                     "3": "声音日记"
+	 *                 }
+	 *             ]
+	 *         },
+	 *         "2": {
+	 *             "name": "播讲",
+	 *             "limit": "2",
+	 *             "list": [
+	 *                 {
+	 *                     "8": "体育"
+	 *                 },
+	 *                 {
+	 *                     "7": "广告"
+	 *                 }
+	 *             ]
+	 *         }
+	 *     },
+	 *     "status": 0,
+	 *     "message": "成功"
+	 * }
+	 *
+	 * @apiErrorExample {json} Error-Response:
+	 * {
+	 * 	   "data": "",
+	 *     "status": -1,
+	 *     "message": "签名校验错误"
+	 * }
+	 */
+	public function index()
+	{
+		$ret = array();
+		$deleted = (int)$this->input->get('deleted');
+		if($this->user_id){
+			$this->db->select('id,name,pid,limit');
+		}
+		$order_by = array('pid' => 'asc', 'sort' => 'desc', 'id' => 'desc');
+		$tag = $this->Album_tag_model->order_by($order_by)->get_many_by('deleted', $deleted);
+		if($tag){
+			foreach($tag as $item){
+				if($item['pid'] == 0){
+					$ret[$item['id']] = $this->user_id ? array('name' => $item['name'], 'limit' => $item['limit']) : $item;
+					$ret[$item['id']]['list'] = array();
+				}else{
+					if(isset($ret[$item['pid']])){
+						$ret[$item['pid']]['list'][] = $this->user_id ? array($item['id'] => $item['name']) : $item;
+					}
+				}
+			}
+		}
+		$this->ajaxReturn($ret);
+	}
+
+	// 查看
+	public function view()
+	{
+
+	}
+
+	/**
+	 * @api {post} /api/album_tag/save 专辑标签-编辑 OR 新增
+	 * @apiVersion 1.0.0
+	 * @apiName album_tag_save
+	 * @apiGroup admin
+	 *
+	 * @apiSampleRequest /api/album_tag/save
+	 *
+	 * @apiParam {Number} admin_id 管理员唯一ID
+	 * @apiParam {String} account 登录账号
+	 * @apiParam {String} sign 校验签名
+	 * @apiParam {Number} id 记录唯一ID 0表示新增 其他表示编辑
+	 * @apiParam {String} name 专辑标签名称
+	 * @apiParam {Number} sort 排序 降序排列
+	 * @apiParam {Number} enable 启用 1是 0否
+	 * @apiParam {Number} deleted 是否删除 1是 0否（为1时其他字段可不传）
+	 * @apiParam {Number} pid 父级ID 0顶级
+	 * @apiParam {Number} limit 限制选择数量
+	 *
+	 * @apiSuccess {Number} status 接口状态 0成功 其他异常
+	 * @apiSuccess {String} message 接口信息描述
+	 * @apiSuccess {String} data 接口数据集
+	 *
+	 * @apiSuccessExample {json} Success-Response:
+	 * {
+	 *	    "data": "",
+	 *	    "status": 0,
+	 *	    "message": ""
+	 *	}
+	 *
+	 * @apiErrorExample {json} Error-Response:
+	 * {
+	 * 	   "data": "",
+	 *     "status": -1,
+	 *     "message": "签名校验错误"
+	 * }
+	 */
+	public function save()
+	{
+		$this->check_operation();
+		$id = (int)$this->input->get_post('id');
+		if($id){
+			$params = elements(
+				array(
+					'name', 'sort', 'deleted', 'enable', 'pid', 'limit'
+				),
+				$this->input->post(),
+				UPDATE_VALID
+			);
+			$this->check_params('edit', $params);
+			if($params['deleted'] == 1){
+				$update = array('deleted' => 1, 'enable' => 0);
+				$flag = $this->Album_tag_model->update($id, $update);
+			}else{
+				unset($params['deleted']);
+				if(isset($params['enable']) && $params['enable']){
+					$params['deleted'] = 0;
+				}
+				$flag = $this->Album_tag_model->update($id, $params);
+			}
+		}else{
+			$params = elements(
+				array(
+					'name', 'sort', 'pid', 'limit'
+				),
+				$this->input->post(),
+				UPDATE_VALID
+			);
+			$this->check_params('add', $params);
+			if($flag = $this->Album_tag_model->insert($params)){
+				$id = $flag;
+			}
+		}
+
+		if($flag){
+			$status = 0;
+			$message = '成功';
+		}else{
+			$status = 1;
+			$message = '失败';
+		}
+		$this->ajaxReturn(array('id' => $id), $status, '操作'.$message);
+	}
+
+	protected function check_params($act, $params)
+	{
+		switch($act){
+			case 'add':
+				if($params['name'] === '' || $params['name'] == UPDATE_VALID){
+					$this->ajaxReturn([], 501, '名称参数错误');
+				}
+				break;
+			case 'edit':
+				break;
+		}
+	}
+}
