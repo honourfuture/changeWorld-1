78ce3dd0fc25a66b6f5e342386e96956038eadf4
代汉桥 <dhq@daihanqiaodeMacBook-Pro.local>
2018-03-11 23:22:43 +0800

    文档更新

--- wap/src/panels/SelectAddress/index.js
+++ wap/src/panels/SelectAddress/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import {action} from 'mobx';
-import {BaseComponent,Base} from '../../common';
-import {Button,List,Radio,WhiteSpace,WingBlank,NavBar} from 'antd-mobile';
+import { action } from 'mobx';
+import { BaseComponent, Base } from '../../common';
+import { Button, List, Radio, WhiteSpace, WingBlank, NavBar } from 'antd-mobile';
 import './SelectAddress.less';
 
-import {icon} from '../../images';
+import { icon } from '../../images';
 
 const Item = List.Item;
 const Brief = Item.Brief;
 
 class AddressItem extends BaseComponent {
     @action.bound
-    changeHandler(){
-        const {index,callBack} = this.props;
+    changeHandler() {
+        const { index, callBack } = this.props;
         callBack && callBack(index);
     }
-    render(){
-        const {name,tel,address,checked} = this.props;
-        return(
+    render() {
+        const { name, tel, address, checked } = this.props;
+        return (
             <div>
                 <List className="addressItem" onClick={this.changeHandler}>
                     <Item
@@ -36,50 +36,50 @@ class AddressItem extends BaseComponent {
     }
 }
 
-export default class SelectAddress extends BaseComponent{
-    store={
-        curIndex:0,
-        addressDate:[{
-            name:"李娟",
-            tel:"182****3679",
-            address:"浙江省-杭州市-西湖区 春申街西溪花园凌波苑春申街西溪花园凌波苑春申街西溪花园凌波苑"
-        },{
-            name:"Kaden McCullough",
-            tel:"134****2587",
-            address:"浙江省-杭州市-西湖区 春申街西溪花园凌波苑春申街西溪花园凌波苑春申街西溪花园凌波苑"
-        },{
-            name:"万莎莎",
-            tel:"134****2587",
-            address:"浙江省-杭州市-西湖区 春申街西溪花园凌波苑春申街西溪花园凌波苑春申街西溪花园凌波苑"
+export default class SelectAddress extends BaseComponent {
+    store = {
+        curIndex: 0,
+        addressDate: [{
+            name: "李娟",
+            tel: "182****3679",
+            address: "浙江省-杭州市-西湖区 春申街西溪花园凌波苑春申街西溪花园凌波苑春申街西溪花园凌波苑"
+        }, {
+            name: "Kaden McCullough",
+            tel: "134****2587",
+            address: "浙江省-杭州市-西湖区 春申街西溪花园凌波苑春申街西溪花园凌波苑春申街西溪花园凌波苑"
+        }, {
+            name: "万莎莎",
+            tel: "134****2587",
+            address: "浙江省-杭州市-西湖区 春申街西溪花园凌波苑春申街西溪花园凌波苑春申街西溪花园凌波苑"
         }]
     }
-    componentDidMount(){
+    componentDidMount() {
     }
     @action.bound
-    changeHandler(index){
+    changeHandler(index) {
         this.store.curIndex = index;
     }
-    render(){
-        const {curIndex,addressDate} = this.store;
-        const addressItem = addressDate.map((item,index)=>{
-            return <AddressItem key={index} checked={curIndex === index} index={index} {...item} callBack={this.changeHandler}/>;
+    render() {
+        const { curIndex, addressDate } = this.store;
+        const addressItem = addressDate.map((item, index) => {
+            return <AddressItem key={index} checked={curIndex === index} index={index} {...item} callBack={this.changeHandler} />;
         });
         return (
             <div className='SelectAddress'>
                 <NavBar
                     className="base-line"
                     mode="light"
-                    icon={<img src={icon.back} alt=''/>}
+                    icon={<img src={icon.back} alt='' />}
                     onLeftClick={Base.goBack}
-                    rightContent={<div onClick={()=>Base.push('NewAddress')} className='right-label'>添加</div>}
-                >支付</NavBar>
+                    rightContent={<div onClick={() => Base.push('NewAddress')} className='right-label'>添加</div>}
+                >选择地址</NavBar>
                 <div className="base-content">
                     <div className="SelectAddress-box">
                         {addressItem}
                     </div>
                     <WhiteSpace size="xl" />
                     <WingBlank>
-                        <Button type="warning" className="save-address" onClick={()=>Base.push('NewAddress')}>+ 新增地址</Button>
+                        <Button type="warning" className="save-address" onClick={() => Base.push('NewAddress')}>+ 新增地址</Button>
                     </WingBlank>
                 </div>
             </div>
