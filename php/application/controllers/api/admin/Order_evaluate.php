8eb433f5ce1c1a350b3c10cdf82dbdb43185c8a7
35945042 <webljx@gmail.com>
2018-03-31 23:29:16 +0800

    订单评论

--- php/application/controllers/api/admin/Order_evaluate.php
+++ php/application/controllers/api/admin/Order_evaluate.php
@@ -12,11 +12,11 @@ class Order_evaluate extends API_Controller {
         parent::__construct();
 
         // $this->check_operation();
-        $this->load->model('Users_model');
+        $this->load->model('Order_evaluate_model');
     }
 
     /**
-	 * @api {get} /api/admin/order_evaluate 订单评价-列表
+	 * @api {get} /api/admin/order_evaluate 评价管理-列表
 	 * @apiVersion 1.0.0
 	 * @apiName order_evaluate
 	 * @apiGroup admin
@@ -26,6 +26,7 @@ class Order_evaluate extends API_Controller {
 	 * @apiParam {Number} admin_id 管理员唯一ID
 	 * @apiParam {String} account 登录账号
 	 * @apiParam {String} sign 校验签名
+	 * @apiParam {Number} status -1全部
 	 *
 	 * @apiSuccess {Number} status 接口状态 0成功 其他异常
 	 * @apiSuccess {String} message 接口信息描述
@@ -47,63 +48,48 @@ class Order_evaluate extends API_Controller {
 	 */
 	public function index()
 	{
-		$ret = ['list' => []];
+		$ret = ['list' => [], 'user' => []];
 
-		$this->load->model('Users_model');
-		$ret['sex'] = $this->Users_model->sex();
+		$ret['status'] = $this->Order_evaluate_model->status();
 
 		$where = [];
-		$where['1 >'] = 0;
+		$status = $this->input->get_post('status');
+		if(isset($ret['status'][$status])){
+			$where['status'] = $status;
+		}else{
+			$where['1 >'] = 0;
+		}
 
 		$order_by = array('id' => 'desc');
-		$ret['count'] = $this->Users_model->count_by($where);
+		$ret['count'] = $this->Order_evaluate_model->count_by($where);
 		if($ret['count']){
-			$this->db->select('id,created_at,updated_at,mobi,account,header,nickname,v,anchor,seller,exp,reg_ip,balance,point,gold');
-			$ret['list'] = $this->Users_model->order_by($order_by)->limit($this->per_page, $this->offset)->get_many_by($where);
+			$this->db->select('id,created_at,status,order_id,order_sn,user_id,seller_uid,remark,photos,is_anonymous');
+			$ret['list'] = $this->Order_evaluate_model->order_by($order_by)->limit($this->per_page, $this->offset)->get_many_by($where);
+
+			$a_uid = $a_order = [];
+			foreach($ret['list'] as $item){
+				$a_order[] = $item['order_id'];
+				$a_uid[] = $item['seller_uid'];
+				$a_uid[] = $item['user_id'];
+			}
+			$k_user = [];
+			//用户信息
+			$this->load->model('Users_model');
+			$this->db->select('id,mobi,header,nickname,v,exp,sex,balance,point,gold');
+			$users = $this->Users_model->get_many($a_uid);
+			foreach($users as $item){
+				$ret['user'][$item['id']] = $item;
+			}
+			//订单信息
+			$this->load->model('Order_model');
+			$this->db->select('id,total_amount,real_total_amount');
+			$order = $this->Order_model->get_many($a_order);
+			foreach($order as $item){
+				$ret['order'][$item['id']] = $item;
+			}
 		}
 
 		$this->ajaxReturn($ret);
-	}
-
-	/**
-	 * @api {get} /api/admin/order_evaluate/view 订单评价-详情
-	 * @apiVersion 1.0.0
-	 * @apiName order_evaluate_view
-	 * @apiGroup admin
-	 *
-	 * @apiSampleRequest /api/admin/order_evaluate/view
-	 *
-	 * @apiParam {Number} admin_id 管理员唯一ID
-	 * @apiParam {String} account 登录账号
-	 * @apiParam {String} sign 校验签名
-	 * @apiParam {Number} id 用户唯一ID
-	 *
-	 * @apiSuccess {Number} status 接口状态 0成功 其他异常
-	 * @apiSuccess {String} message 接口信息描述
-	 * @apiSuccess {Object} data 接口数据集
-	 *
-	 * @apiSuccessExample {json} Success-Response:
-	 * {
-	 *     "data": {},
-	 *     "status": 0,
-	 *     "message": "成功"
-	 * }
-	 *
-	 * @apiErrorExample {json} Error-Response:
-	 * {
-	 * 	   "data": "",
-	 *     "status": -1,
-	 *     "message": "签名校验错误"
-	 * }
-	 */
-	public function view()
-	{
-		$id = $this->input->get_post('id');
-		if($info = $this->Users_model->get($id)){
-			$this->ajaxReturn($info);
-		}else{
-			$this->ajaxReturn([], 1, '查看用户详情ID错误');
-		}
 	}
 
 	protected function check_params($act, $params)
