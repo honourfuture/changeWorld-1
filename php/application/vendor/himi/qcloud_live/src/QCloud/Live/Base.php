694c346ad2c69d9adc585d96a13e3e5d28bb5f70
35945042 <webljx@gmail.com>
2018-04-24 15:36:54 +0800

    php版本兼容问题

--- php/application/vendor/himi/qcloud_live/src/QCloud/Live/Base.php
+++ php/application/vendor/himi/qcloud_live/src/QCloud/Live/Base.php
@@ -13,29 +13,29 @@ class Base
     protected $bizid = '6077';
     # 接口url
     protected $url = '';
-    # 统计URL 
+    # 统计URL
     protected $stat_url = '';
     # 视频直播过期时间
-    protected $expire = 24 * 3600;
-    protected $time = 0;
+    protected $expire = 86400;
+    protected $time   = 0;
 
     public function setAppInfo($appid, $api_key, $push_key, $bizid, $url = '', $stat_url = '')
     {
-        $this->appid = $appid;
-        $this->api_key = $api_key;
+        $this->appid    = $appid;
+        $this->api_key  = $api_key;
         $this->push_key = $push_key;
-        $this->bizid = $bizid;
-        $this->url = $url == '' ? 'http://fcgi.video.qcloud.com/common_access' : $url;
+        $this->bizid    = $bizid;
+        $this->url      = $url == '' ? 'http://fcgi.video.qcloud.com/common_access' : $url;
         $this->stat_url = $stat_url == '' ? 'http://statcgi.video.qcloud.com/common_access' : $stat_url;
     }
-    
+
     /**
      *  获取签名
-     * 
+     *
      */
     public function getSign()
     {
         $this->time = strtotime('10 minute');
-        return md5($this->api_key.$this->time);
+        return md5($this->api_key . $this->time);
     }
 }
