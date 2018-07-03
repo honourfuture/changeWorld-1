ad9a3afdfde2e29ea05a24cea82b36e1e432d12e
35945042 <webljx@gmail.com>
2018-07-03 18:53:13 +0800

    flex

--- php/application/controllers/api/admin/Headhunter.php
+++ php/application/controllers/api/admin/Headhunter.php
@@ -202,4 +202,52 @@ class Headhunter extends API_Controller {
 			$this->ajaxReturn([], 1, '参数错误');
 		}
 	}
+
+	/**
+	 * @api {post} /api/admin/headhunter/onoff 猎头用户-开关
+	 * @apiVersion 1.0.0
+	 * @apiName headhunter_onoff
+	 * @apiGroup admin
+	 *
+	 * @apiSampleRequest /api/admin/headhunter/onoff
+	 *
+	 * @apiParam {Number} admin_id 管理员唯一ID
+	 * @apiParam {String} account 登录账号
+	 * @apiParam {String} sign 校验签名
+	 * @apiParam {String} id 记录唯一ID
+	 * @apiParam {String} headhunter 猎头 0否 1是
+	 *
+	 * @apiSuccess {Number} status 接口状态 0成功 其他异常
+	 * @apiSuccess {String} message 接口信息描述
+	 * @apiSuccess {String} data 接口数据集
+	 *
+	 * @apiSuccessExample {json} Success-Response:
+	 * {
+	 *	    "data": "",
+	 *	    "status": 0,
+	 *	    "message": ""
+	 *	}
+	 *
+	 * @apiErrorExample {json} Error-Response:
+	 * {
+	 * 	   "data": "",
+	 *     "status": -1,
+	 *     "message": "签名校验错误"
+	 * }
+	 */
+	public function onoff()
+	{
+		$id = (int)$this->input->get_post('id');
+		$headhunter = (int)$this->input->get_post('headhunter');
+		if($id){
+			$this->load->model('Users_model');
+			if($this->Users_model->update($id, ['headhunter' => $headhunter])){
+				$this->ajaxReturn();
+			}else{
+				$this->ajaxReturn([], 2, '设置失败');
+			}
+		}else{
+			$this->ajaxReturn([], 1, '参数错误');
+		}
+	}
 }
