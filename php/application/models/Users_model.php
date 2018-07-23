cf6efb8dbd09086d37fea746270ffad933ffb47a
35945042 <webljx@gmail.com>
2018-07-23 10:15:40 +0800

    pretty id

--- php/application/models/Users_model.php
+++ php/application/models/Users_model.php
@@ -12,7 +12,7 @@ class Users_model extends MY_Model
     public $primary_key   = 'id';
     public $before_create = array('created_at', 'updated_at', 'updated_valid');
     public $before_update = array('updated_at', 'updated_valid');
-    public $protected_attributes = array('id');
+    public $protected_attributes = array('v');
 
     private $_pwd_key = '5jGwhZLONPxb';
 
