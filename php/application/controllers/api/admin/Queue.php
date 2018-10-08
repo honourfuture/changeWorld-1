d3633cfb5d230891acb0e056dceda4e9117c246e
35945042 <webljx@gmail.com>
2018-10-08 12:52:15 +0800

    fix

--- php/application/controllers/api/admin/Queue.php
+++ php/application/controllers/api/admin/Queue.php
@@ -63,6 +63,24 @@ class Queue extends API_Controller {
 
     	if($ret['count'] > 0){
 	    	switch ($this->task) {
+	    		case 'audio_comment':
+	    			$list = $this->Queue_model->order_by('id', 'desc')->limit($this->per_page, $this->offset)->get_many_by($where);
+	    			if($list){
+	    				$this->load->model('Room_audio_model');
+	    				foreach($list as $item){
+	    					$item['params'] = json_decode($item['params'], true);
+
+	    					$item['play_times'] = 0;
+	    					$item['video_url'] = $item['title'] = '';
+
+	    					$this->db->select('play_times,video_url,title');
+	    					if($audio = $this->Room_audio_model->get($item['params']['id'])){
+	    						$item = array_merge($item, $audio);
+	    					}
+	    					$ret['list'][] = $item;
+	    				}
+	    			}
+	    			break;
 	    		case 'fans':
 	    			$list = $this->Queue_model->order_by('id', 'desc')->limit($this->per_page, $this->offset)->get_many_by($where);
 	    			if($list){
