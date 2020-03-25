e46e1f5a3c109b57f451c6f6c3b3ab57e34a635f
xielongrui <ruilongd@126.com>
2020-03-25 10:23:30 +0800

    fix grade show error

--- php/application/models/Grade_model.php
+++ php/application/models/Grade_model.php
@@ -57,8 +57,7 @@ class Grade_model extends MY_Model
     public function expDiff($exp)
     {
         $ret = [];
-        $order_by = array('grade_demand' => 'asc');
-        $grades = $this->db->where('deleted', 0)->where('status', 0)->where('enable', 1)->select('*')->get($this->table())->order_by($order_by)->result_array();
+        $grades = $this->db->where('deleted', 0)->where('status', 0)->where('enable', 1)->select('*')->get($this->table())->result_array();
         $arrGrades = [];
         foreach ($grades as $key => $grade) {
             $arrGrades["level_{$grade['grade_name']}"] = $grade;
