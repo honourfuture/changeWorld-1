9482e1bf807be93a77f6475d503829ae510f19c8
xielongrui <ruilongd@126.com>
2020-04-11 14:59:33 +0800

    已修改        php/application/helpers/MY_array_helper.php

--- php/application/helpers/MY_array_helper.php
+++ php/application/helpers/MY_array_helper.php
@@ -1,7 +1,6 @@
 <?php
-if( !function_exists('numberFormat') ) {
-	function numberFormat($number, $decimals = 2)
-	{
-		return number_format($number, $decimals, '.', '');
-	}
+
+function numberFormat($number, $decimals = 2)
+{
+	return number_format($number, $decimals, '.', '');
 }
