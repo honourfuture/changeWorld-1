8ef4549e91bbb1fc180e8e104d0721df718a4a58
root <admin@example.com>
2020-05-11 23:11:11 +0800

    修正我的等级展示

--- php/application/controllers/api/Grade.php
+++ php/application/controllers/api/Grade.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 class Grade extends API_Controller {
 
-	public function __construct()
+    public function __construct()
     {
         parent::__construct();
         $this->load->model('Grade_model');
@@ -15,39 +15,39 @@ class Grade extends API_Controller {
     }
 
     /**
-	 * @api {get} /api/grade 等级经验-我的等级
-	 * @apiVersion 1.0.0
-	 * @apiName grade
-	 * @apiGroup api
-	 *
-	 * @apiSampleRequest /api/grade
-	 *
-	 * @apiParam {Number} user_id 用户唯一ID
-	 * @apiParam {String} sign 校验签名
-	 *
-	 * @apiSuccess {Number} status 接口状态 0成功 其他异常
-	 * @apiSuccess {String} message 接口信息描述
-	 * @apiSuccess {Object} data 接口数据集
-	 * @apiSuccess {Object} data.grade 我的等级
-	 * @apiSuccess {String} data.grade.before_grade_name 前一级等级
-	 * @apiSuccess {String} data.grade.grade_name 当前等级
-	 * @apiSuccess {String} data.grade.after_grade_name 下一级等级
-	 * @apiSuccess {String} data.grade.diff 升级差值
-	 * @apiSuccess {String} data.grade.exp 当前经验值
-	 * @apiSuccess {String} data.level.rule 权益说明
-	 * @apiSuccess {String} data.level.icon 图标
-	 * @apiSuccess {String} data.level.remark 等级说明
-	 *
-	 * @apiSuccessExample {json} Success-Response:
-	 * {
-	 *     "data": {
-	 *         "grade": {
-	 *             "before_grade_name": "",
-	 *             "grade_name": "",
-	 *             "after_grade_name": "1",
-	 *             "diff": 10000,
-	 *             "exp": "0"
-	 *         },
+     * @api {get} /api/grade 等级经验-我的等级
+     * @apiVersion 1.0.0
+     * @apiName grade
+     * @apiGroup api
+     *
+     * @apiSampleRequest /api/grade
+     *
+     * @apiParam {Number} user_id 用户唯一ID
+     * @apiParam {String} sign 校验签名
+     *
+     * @apiSuccess {Number} status 接口状态 0成功 其他异常
+     * @apiSuccess {String} message 接口信息描述
+     * @apiSuccess {Object} data 接口数据集
+     * @apiSuccess {Object} data.grade 我的等级
+     * @apiSuccess {String} data.grade.before_grade_name 前一级等级
+     * @apiSuccess {String} data.grade.grade_name 当前等级
+     * @apiSuccess {String} data.grade.after_grade_name 下一级等级
+     * @apiSuccess {String} data.grade.diff 升级差值
+     * @apiSuccess {String} data.grade.exp 当前经验值
+     * @apiSuccess {String} data.level.rule 权益说明
+     * @apiSuccess {String} data.level.icon 图标
+     * @apiSuccess {String} data.level.remark 等级说明
+     *
+     * @apiSuccessExample {json} Success-Response:
+     * {
+     *     "data": {
+     *         "grade": {
+     *             "before_grade_name": "",
+     *             "grade_name": "",
+     *             "after_grade_name": "1",
+     *             "diff": 10000,
+     *             "exp": "0"
+     *         },
      *      "rank": [
      *          {
      *             "id": "1",
@@ -68,75 +68,62 @@ class Grade extends API_Controller {
      *             "isCheck": "是否选中 1选中 有的数组里会没有",
      *          },
      *         ]
-	 *     },
-	 *     "status": 0,
-	 *     "message": "成功"
-	 * }
-	 *
-	 * @apiErrorExample {json} Error-Response:
-	 * {
-	 * 	   "data": "",
-	 *     "status": -1,
-	 *     "message": "签名校验错误"
-	 * }
-	 */
-	public function index()
-	{
-		$ret = [];
+     *     },
+     *     "status": 0,
+     *     "message": "成功"
+     * }
+     *
+     * @apiErrorExample {json} Error-Response:
+     * {
+     * 	   "data": "",
+     *     "status": -1,
+     *     "message": "签名校验错误"
+     * }
+     */
+    public function index()
+    {
+        $ret = [];
 
-		$user = $this->get_user();
+        $user = $this->get_user();
         $this->load->model('Users_model');
-		$this->load->model('Grade_model');
+        $this->load->model('Grade_model');
         $this->load->model('Rank_rule_model');
         $this->load->model('Users_rank_rule_verify_model');
 
 
         $exp = $user['exp'];
-		$arrGrade = $this->Grade_model->getExpDiff($exp);
+        $arrGrade = $this->Grade_model->getExpDiff($exp);
         $ret['grade'] = $arrGrade;
 
         $resultRank = [];
         $user = $this->Users_model->get($this->user_id);
         $thisRank = $user['rank_rule_id'] ? $user['rank_rule_id'] : 1;
+        $nextRank = ( $thisRank >=4 ? 5 : ($thisRank + 1) );
         $ranks = $this->Rank_rule_model->getAllByWhere(['status' => 0]);
-        $rankIdKey = [];
+        $rankList = [];
         foreach ($ranks as $rank){
             $rank['icon'] = config_item('base_url') .$rank['icon'];
-            $rankIdKey[$rank['id']] = $rank;
+            $rank['isCheck'] = ($rank['id'] == $thisRank ? 1 : 0);
+            $rank['isShowUp'] = 0;
+            $rankList["rank_{$rank['id']}"] = $rank;
         }
         $verify = $this->Users_rank_rule_verify_model->order_by('id', 'desc')->get_by(['from' => $thisRank, 'user_id' => $this->user_id]);
-        $isShow = 0;
 
         //0 未审核 1 审核中 2 审核通过 3 拒绝
-        if(isset($verify['status'])){
-            $isShow = ($verify['status'] == 3 ? 1 : 0);
+        if( isset($rankList["rank_{$nextRank}"]) && $rankList["rank_{$nextRank}"]['exp'] <= $exp){
+            $rankList["rank_{$thisRank}"]['isShowUp'] = (isset($verify['status']) && $verify['status'] == 3 ) ? 1 : 0; 
         }
-        if( isset($rankIdKey[$thisRank + 1]) && $rankIdKey[$thisRank + 1]['exp'] <= $exp){
-            $rankIdKey[$thisRank]['isShowUp'] = $isShow; 
+        if( $thisRank<=2 ){
+            unset($rankList["rank_3"], $rankList["rank_4"], $rankList["rank_5"]);
         }
-
-        $rankIdKey[$thisRank]['isCheck'] = 1;
-
-        switch ($thisRank){
-            case 1:
-                $resultRank[] = $rankIdKey[$thisRank];
-                $resultRank[] = $rankIdKey[$thisRank + 1];
-                break;
-            case 2:
-                if($isShow){
-                    $resultRank[] = $rankIdKey[$thisRank];
-                    $resultRank[] = $rankIdKey[$thisRank + 1];
-                }else{
-                    $resultRank[] = $rankIdKey[$thisRank];
-                }
-                break;
-            default:
-                $resultRank = [$rankIdKey['3'], $rankIdKey['4'], $rankIdKey['5']];
+        else{
+            unset($rankList["rank_1"], $rankList["rank_2"]);
         }
 
-        $ret['rank'] = $resultRank;
-		$this->ajaxReturn($ret);
-	}
+        ksort($rankList);
+        $ret['rank'] = array_values($rankList);
+        $this->ajaxReturn($ret);
+    }
 
     /**
      * @api {get} /api/grade/level 会员等级-列表
