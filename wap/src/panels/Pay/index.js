e8c749809f47ad7a1efe266e6da2d15468e59b86
xielongrui <ruilongd@126.com>
2020-05-16 11:38:36 +0800

    修正支付积分结算

--- wap/src/panels/Pay/index.js
+++ wap/src/panels/Pay/index.js
@@ -26,21 +26,24 @@ export default class Pay extends BaseComponent {
             }
         ],
         curIndex: 0,
+        payType: 'balance',
         data: {}
     };
     @action.bound
-    changeHandler(index) {
+    changeHandler(index, payType) {
         this.store.curIndex = index;
+        this.store.payType = payType;
     }
     @action.bound
     payHandler() {
         const { pretty_id, id, isPretty } = Base.getPageParams();
         const { curIndex, payTypeList } = this.store;
         const { type } = payTypeList[curIndex];
+        this.store.payType = type;
         const { pay_sn, order_sn } = Base.getPageParams();
         const trade_type = pay_sn ? "pay_sn" : "order_sn";
         const trade_sn = pay_sn ? pay_sn : order_sn;
-        const { real_total_amount, balance } = this.store.data;
+        const { real_total_amount, balance, price_point } = this.store.data;
         if (parseInt(isPretty)) {
             Base.POST(
                 {
@@ -141,7 +144,8 @@ export default class Pay extends BaseComponent {
             Base.GET({ act: "pretty", op: "view", mod: "user", id }, res => {
                 this.store.data = {
                     ...res.data,
-                    real_total_amount: res.data.price
+                    real_total_amount: res.data.price,
+                    price_point: res.data.price_point
                 };
             });
         } else {
@@ -160,104 +164,101 @@ export default class Pay extends BaseComponent {
         }
     }
     render() {
-        console.log(Base.getPageParams())
         const { pay_sn, order_sn, id, isPretty } = Base.getPageParams();
         const { payTypeList, curIndex, data } = this.store;
-        const { real_total_amount, balance, point } = data;
+        const { real_total_amount, balance, point, price_point } = data;
         const payTypes = payTypeList.map((item, index) => {
-//          if(index == 3 && !isPretty){
-//              return ;
-//          }
+            if(index == 3 && !isPretty){
+                return ;
+            }
             const { type } = item;
-            console.log(type,'9996')
             if(type === 'point' && isPretty){
                 return (
                     <Flex
-                justify="between"
-                key={type}
-                onClick={() => this.changeHandler(index)}
-            >
-            <Flex.Item>
-                <span className="pay-gold">积分支付</span>
-                    <span className="account-money">
-                    积分余额：{Base.getNumFormat(point)}
-            </span>
-                </Flex.Item>
-                <Flex.Item>
-                <Radio checked={curIndex === index} name={type} />
-                </Flex.Item>
-                </Flex>
-            );
+                        justify="between"
+                        key={type}
+                        onClick={() => this.changeHandler(index, 'point')}
+                    >
+                        <Flex.Item>
+                            <span className="pay-gold">积分支付</span>
+                            <span className="account-money">
+                                积分余额：{Base.getNumFormat(point, 0)}
+                            </span>
+                        </Flex.Item>
+                        <Flex.Item>
+                            <Radio checked={curIndex === index} name={type} />
+                        </Flex.Item>
+                    </Flex>
+                );
             }else  if (type === "balance") {
                 return (
                     <Flex
-                justify="between"
-                key={type}
-                onClick={() => this.changeHandler(index)}
-            >
-            <Flex.Item>
-                <span className="pay-gold">余额支付</span>
-                    <span className="account-money">
-                    账户余额：￥{Base.getNumFormat(balance)}
-            </span>
-                </Flex.Item>
-                <Flex.Item>
-                <Radio checked={curIndex === index} name={type} />
-                </Flex.Item>
-                </Flex>
-
-            );
+                        justify="between"
+                        key={type}
+                        onClick={() => this.changeHandler(index, 'balance')}
+                    >
+                        <Flex.Item>
+                            <span className="pay-gold">余额支付</span>
+                            <span className="account-money">
+                                账户余额：￥{Base.getNumFormat(balance)}
+                            </span>
+                        </Flex.Item>
+                        <Flex.Item>
+                            <Radio checked={curIndex === index} name={type} />
+                        </Flex.Item>
+                    </Flex>
+                );
             } else {
                 return (
                     <Flex
-                justify="between"
-                key={type}
-                onClick={() => this.changeHandler(index)}
-            >
-            <Flex.Item>
-                <img src={payIconDic[type]} alt="" />
-                    </Flex.Item>
-                    <Flex.Item>
-                    <Radio checked={curIndex === index} name={type} />
-                </Flex.Item>
-                </Flex>
-            );
+                        justify="between"
+                        key={type}
+                        onClick={() => this.changeHandler(index, type)}
+                    >
+                        <Flex.Item>
+                            <img src={payIconDic[type]} alt="" />
+                        </Flex.Item>
+                        <Flex.Item>
+                            <Radio checked={curIndex === index} name={type} />
+                        </Flex.Item>
+                    </Flex>
+                );
             }
         });
         return (
             <div className="Pay">
-            <NavBar
-        className="base-line"
-        mode="light"
-        icon={<img src={icon.back} alt="" />}
-        onLeftClick={Base.goBack}
-            >
-            支付
-            </NavBar>
-            <div className="base-content">
-            <div className="Pay-box">
-            <div className="Pay-box-mode">
-            <h4 className="pay-h4">支付方式</h4>
-            <div className="pay-check">
-            {payTypes}
-            <span className="payMoney">
-        <em>{+curIndex !== 3 ? '支付金额：' : '积分支付:'}</em>
-        ￥{curIndex !==3? Base.getNumFormat(real_total_amount):'131456'}
-    </span>
-        </div>
-        </div>
-        <Flex
-        justify="center"
-        align="center"
-        className="Pay-box-opear"
-            >
-            <Button onClick={this.payHandler} type="warning">
-            下一步
-            </Button>
-            </Flex>
+                <NavBar
+                    className="base-line"
+                    mode="light"
+                    icon={<img src={icon.back} alt="" />}
+                    onLeftClick={Base.goBack}
+                >
+                    支付
+                </NavBar>
+                <div className="base-content">
+                    <div className="Pay-box">
+                        <div className="Pay-box-mode">
+                            <h4 className="pay-h4">支付方式</h4>
+                            <div className="pay-check">
+                                {payTypes}
+                                <span className="payMoney">
+                                <em>{this.store.payType == 'point' ? '抵扣积分：' : '支付金额:'}</em>
+                                {this.store.payType != 'point' ?  ('￥' + Base.getNumFormat(real_total_amount) ) : Base.getNumFormat(price_point, 0)}
+                                </span>
+                            </div>
+                        </div>
+                        <Flex
+                            justify="center"
+                            align="center"
+                            className="Pay-box-opear"
+                        >
+                            <Button onClick={this.payHandler} type="warning">
+                                下一步
+                            </Button>
+                        </Flex>
+                    </div>
+                </div>
             </div>
-            </div>
-            </div>
-    );
+        );
     }
 }
